45fb1ff302714b84e511fc86f15dfe432c89151c
AyaanK2 <ayaan18028@iiitd.ac.in>
2020-04-30 01:46:09 +0530

    FINAL COMMIT

--- MainApp/app/__init__.py
+++ MainApp/app/__init__.py
@@ -1,6 +1,10 @@
 from flask import Flask
 from os import urandom
 import mysql.connector
+from datetime import date
+import atexit
+
+from apscheduler.schedulers.background import BackgroundScheduler
 
 db = mysql.connector.connect(
   host="localhost",
@@ -10,6 +14,32 @@ db = mysql.connector.connect(
 )
 
 app = None
+
+def updateDues():
+  currDate = date.today()
+  if currDate > app.config['currDate']:
+    dbCursor = db.cursor()
+    sql = "UPDATE (insurances A INNER JOIN policies B ON A.policy_key = B.policy_key) \
+      INNER JOIN clients C on A.client_ID = C.client_ID \
+      SET A.dues=A.dues+B.ppm WHERE TIMESTAMPDIFF(MONTH, A.start_date, curdate()) > \
+      TIMESTAMPDIFF(MONTH, A.start_date, DATE_SUB(curdate(), INTERVAL 1 DAY)) AND \
+      DATE_ADD(start_date, INTERVAL B.duration YEAR) >= curdate() AND C.company_reg_no IS NULL;"
+    dbCursor.execute(sql)
+    db.commit()
+    sql = "UPDATE (((insurances A INNER JOIN policies B ON A.policy_key = B.policy_key) \
+      INNER JOIN clients C on A.client_ID = C.client_ID) \
+      INNER JOIN companies D ON D.company_reg_no = C.company_reg_no) \
+      INNER JOIN offers E on D.company_ID = E.company_ID \
+      SET A.dues=A.dues+(B.ppm*(100-E.discount_factor)/100) \
+      WHERE TIMESTAMPDIFF(MONTH, A.start_date, curdate()) > \
+      TIMESTAMPDIFF(MONTH, A.start_date, DATE_SUB(curdate(), INTERVAL 1 DAY)) \
+      AND DATE_ADD(start_date, INTERVAL B.duration YEAR) >= curdate() AND E.active=1;"
+    dbCursor.execute(sql)
+    db.commit()
+    dbCursor.close()
+  app.config['currDate'] = currDate
+    
+
 
 def create_app():
     global app
@@ -35,7 +65,11 @@ def create_app():
     app.register_blueprint(organizations)
     
 
-    # from .admin import viewBranchDetails
-    # app.jinja_env.globals.update(viewBranchDetails=viewBranchDetails)
+    scheduler = BackgroundScheduler()
+    scheduler.add_job(func=updateDues, trigger="interval", seconds=5)
+    app.config['currDate'] = date.today()
+  
+    
+    scheduler.start()
 
     return app
